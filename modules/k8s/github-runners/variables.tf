eefb1108e7387c22761fce5c67128d374f4b1106
William <3422794+FriedCircuits@users.noreply.github.com>
2022-10-24 05:34:06 +0000

    Module to deploy Github self-hosted runners

---
+++ modules/k8s/github-runners/variables.tf
@@ -0,0 +1,32 @@
+variable "namespace" {
+  description = "Which Kubernetes Namespace to deploy the chart into."
+  type        = string
+  default     = "github"
+}
+
+variable "cert_chart_verison" {
+  description = "Helm chart version for cert manager. Required for github runners."
+  type        = string
+  default     = "v1.10.0"
+}
+
+variable "github_chart_version" {
+  description = "The version of the helm chart to use. Note that this is different from the app/container version."
+  type        = string
+  default     = "0.21.0"
+}
+
+variable "github_token" {
+  description = "Github token with repo access."
+  type        = string
+}
+
+variable "repos" {
+  description = "List of repos to run runers for."
+  type = list(object({
+    repo = string
+    min  = number
+    max  = number
+  }))
+  default = []
+}
